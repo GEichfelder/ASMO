aa0238ffd66948bf3546abdd32160f8592c2e090
Leo Warnow <git@leo-warnow.net>
2018-11-24 12:54:56 +0100

    Better parameter description
    Added some more description to all parameter files and their options

--- parameter/ps_param_auto.m
+++ parameter/ps_param_auto.m
@@ -1,9 +1,10 @@
 function [adapt,alpha,beta,epsilon,b,r,a_start,a_lim] = ps_param_auto(problem)
 %PS_PARAM_AUTO Some autocomputed parameters to load
 %   This file will compute some parameters based on the problems dimensions
-%   and can be manipulated to use either ps_standard or ps_adapt.
+%   and can be manipulated to use either adaptive (m = 2 only) or
+%   non-adaptive Pascoletti Serafini scalarization.
 
-% Should Pascoletti Serafini use adapted mode?
+% Should Pascoletti Serafini use adapted mode? (1 == yes, 0 == no)
 adapt = 0;
 
 % What is the dimension of our problem?

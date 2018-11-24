e9515c9c54053297f2b69b484a8fba2551794de5
Leo Warnow <git@leo-warnow.net>
2018-11-24 12:57:09 +0100

    Changed comments in UserFile
    For better consistency among all files changed comments in UserFile

--- UserFile.m
+++ UserFile.m
@@ -10,7 +10,7 @@ problem = 'problem_KimDeWeck';
 % Choose a parameter file
 parameter = 'ps_param_auto';
 
-% Should the result be plotted (m=2,3 only) [0==no,1==yes]
+% Should the result be plotted (m = 2 and m = 3 only) [1 == yes, 0 == no]
 plot_result = 1;
 
 %% Please don't touch anything below this line

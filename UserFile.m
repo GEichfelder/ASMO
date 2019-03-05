0249d8348cdbd07b49ceaf9b895e4a4ab1d781d3
Leo Warnow <git@leo-warnow.net>
2019-03-05 12:39:23 +0100

    Added licence notice
    Added notice according to GNU GPL / LGPL advice

--- UserFile.m
+++ UserFile.m
@@ -1,3 +1,22 @@
+%UserFile provides an interface to the included solvers
+% Start by running this file or adjust the specified problem and parameter
+% file names to use the solvers for other problems
+
+%% Copyright (C) 2019 Leo Warnow
+
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published
+% by the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser General Public License for more details.
+
+% You should have received a copy of the GNU Lesser General Public License
+% along with these files.  If not, see https://www.gnu.org/licenses/.
+
 %% Some clean-up first
 clear;
 close all;
--- callSolver.m
+++ callSolver.m
@@ -1,3 +1,18 @@
+% Copyright (C) 2019 Leo Warnow
+
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published
+% by the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser General Public License for more details.
+
+% You should have received a copy of the GNU Lesser General Public License
+% along with these files.  If not, see https://www.gnu.org/licenses/.
+
 function [xres,fres,k] = callSolver(problem_name,parameter_name,plot_result)
 %CALLSOLVER is used as an interface between user and actual solver
 %   This function loads all needed files and calls the solver afterwards
